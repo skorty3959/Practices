60630bfd8a28dde6f5390ae06ec439d3a572dc38
skorty3959 <101987884+skorty3959@users.noreply.github.com>
2022-04-27 19:32:01 +0300

    Add files via upload

---
+++ practic 3(if,else,switch).c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+int main()
+    {
+    int x;
+    printf("¬ведите 1,чтобы сложить два числа,введите 2, чтобы вычесть второе из первого, введите 3, чтобы сравнить два числа и вывести результат:");
+    scanf("%d",&x);
+    switch(x)
+        {
+        case 1:
+        num1+num2
+            break;
+        case 2:
+        num1-num2
+            break;
+        case 3:
+        printf("num1 больше num2")
+            break;
+        default:
+
+            break;
+        }
+    }
